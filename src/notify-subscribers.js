c584654fa9e5e2ccb7e7450f7d0c7f8e395973b7
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2021-07-07 00:25:52 +0100

    checkpoint: start tests after Async state prototype

--- src/notify-subscribers.js
+++
@@ -1,7 +0,0 @@
-import { EMPTY_OBJECT, invokeIfPresent } from "./utils";
-
-export function notifySubscribers(promiseState) {
-  Object.values(promiseState.subscriptions ?? EMPTY_OBJECT).forEach(t => {
-    invokeIfPresent(t.callback, promiseState);
-  });
-}
