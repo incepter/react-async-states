f88fe8fd0bd6565543545208dd2e1c3ed4c45400
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2021-12-14 12:08:49 +0100

    revert deps to react 16

--- packages/example/src/index.js
+++ packages/example/src/index.js
@@ -4,4 +4,5 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.Suspense fallback="app suspended"><App/></React.Suspense>);
+// ReactDOM.createRoot(document.getElementById('root')).render(<React.Suspense fallback="app suspended"><App/></React.Suspense>);
+ReactDOM.render(<App/>, document.getElementById('root'));
