8d2149e25c5f758d533d95fd69b44bdcdef5d9af
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-01-16 20:18:47 +0100

    add builder and move example folder

--- packages/react-async-states/src/hooks/useAsyncState.ts
+++ packages/react-async-states/src/hooks/useAsyncState.ts
@@ -1,10 +1,20 @@
-import {oneObjectIdentity, shallowClone, shallowEqual} from "shared";
+import {oneObjectIdentity, shallowClone, shallowEqual} from "../../../shared";
 import {useAsyncStateImpl} from "./useAsyncStateImpl";
 import {
+  EqualityFn,
   ExtendedUseAsyncStateConfiguration,
+  HoistToProviderConfig,
   PartialUseAsyncStateConfiguration,
   UseAsyncStateReturnValue
 } from "../types.internal";
+import {
+  AsyncStateKey,
+  AsyncStateSource,
+  ForkConfig,
+  Producer,
+  ProducerRunEffects,
+  State
+} from "../../../async-state";
 
 // default
 function useAsyncStateExport<T, E>(
@@ -104,17 +114,6 @@ function useHoistAutoAsyncState<T, E>(
   );
 }
 
-useAsyncStateExport.auto = useAutoAsyncState;
-useAsyncStateExport.lazy = useLazyAsyncState;
-useAsyncStateExport.fork = useForkAsyncState;
-useAsyncStateExport.hoist = useHoistAsyncState;
-useAsyncStateExport.forkAuto = useForkAutoAsyncState;
-useAsyncStateExport.hoistAuto = useHoistAutoAsyncState;
-
-useAsyncStateExport.payload = curryProperty(payloadArgsToOverrides);
-useAsyncStateExport.selector = curryProperty(selectorArgsToOverrides);
-useAsyncStateExport.condition = curryProperty(conditionArgsToOverrides);
-
 function selectorArgsToOverrides(args) {
   const areEqual = args[1] || shallowEqual;
   const selector = args[0] || oneObjectIdentity;
@@ -135,89 +134,113 @@ function curryProperty<T, E>(
   initialOverrides?: PartialUseAsyncStateConfiguration<T, E>
 ) {
   return function constructHookBuilder(...args) {
-    const configOverrides = shallowClone(
-      initialOverrides,
-      argsToOverrides(args)
-    );
 
-    function hookBuilder(
-      subscriptionConfig,
-      dependencies
-    ) {
-      return useAsyncStateImpl(
-        subscriptionConfig,
-        dependencies,
-        configOverrides
-      );
+    const configOverrides = shallowClone(initialOverrides, argsToOverrides(args));
+
+    function hookBuilder(subscriptionConfig, dependencies) {
+      return useAsyncStateImpl(subscriptionConfig, dependencies, configOverrides);
     }
 
-    function curryConfigOverride(
-      prop1,
-      value1?,
-      prop2?,
-      value2?
-    ) {
-      return function hook(
-        subscriptionConfig,
-        dependencies
-      ) {
+    function curryConfigOverride(prop1, value1?, prop2?, value2?) {
+      return function hook(subscriptionConfig, dependencies) {
         configOverrides[prop1] = value1;
         if (prop2 !== undefined) {
           configOverrides[prop2] = value2;
         }
-        return useAsyncStateImpl(
-          subscriptionConfig,
-          dependencies,
-          configOverrides
-        );
+        return useAsyncStateImpl(subscriptionConfig, dependencies, configOverrides);
       }
     }
 
-    hookBuilder.fork = curryConfigOverride(
-      "fork",
-      true
-    );
-    hookBuilder.lazy = curryConfigOverride(
-      "lazy",
-      true
-    );
-    hookBuilder.auto = curryConfigOverride(
-      "lazy",
-      false
-    );
-    hookBuilder.hoist = curryConfigOverride(
-      "hoistToProvider",
-      true
-    );
-    hookBuilder.forkAuto = curryConfigOverride(
-      "fork",
-      true,
-      "lazy",
-      false
-    );
-    hookBuilder.hoistAuto = curryConfigOverride(
-      "hoist",
-      true,
-      "lazy",
-      false
-    );
+    hookBuilder.fork = curryConfigOverride("fork", true);
+    hookBuilder.lazy = curryConfigOverride("lazy", true);
+    hookBuilder.auto = curryConfigOverride("lazy", false);
+    hookBuilder.hoist = curryConfigOverride("hoistToProvider", true);
+    hookBuilder.forkAuto = curryConfigOverride("fork", true, "lazy", false);
+    hookBuilder.hoistAuto = curryConfigOverride("hoist", true, "lazy", false);
 
-    hookBuilder.payload = curryProperty(
-      payloadArgsToOverrides,
-      configOverrides
-    );
-    hookBuilder.selector = curryProperty(
-      selectorArgsToOverrides,
-      configOverrides
-    );
-    hookBuilder.condition = curryProperty(
-      conditionArgsToOverrides,
-      configOverrides
-    );
+    hookBuilder.payload = curryProperty(payloadArgsToOverrides, configOverrides);
+    hookBuilder.selector = curryProperty(selectorArgsToOverrides, configOverrides);
+    hookBuilder.condition = curryProperty(conditionArgsToOverrides, configOverrides);
 
     return Object.freeze(hookBuilder);
   }
 }
+
+type Builder<T, E> = {
+  initialValue: (value: T) => Builder<T, E>,
+  producer: (producer: Producer<T>) => Builder<T, E>
+  key: (key: AsyncStateKey) => Builder<T, E>,
+  source: (src: AsyncStateSource<T>) => Builder<T, E>,
+  runEffect: (effect: ProducerRunEffects) => Builder<T, E>,
+  runEffectDurationMs: (duration: number) => Builder<T, E>,
+  payload: (payload: { [id: string]: any }) => Builder<T, E>,
+  lazy: (lazy: boolean) => Builder<T, E>,
+  fork: (fork: boolean) => Builder<T, E>,
+  condition: (condition: boolean) => Builder<T, E>,
+  hoistToProvider: (hoist: boolean) => Builder<T, E>,
+  forkConfig: (forkConfig: ForkConfig) => Builder<T, E>,
+  hoistToProviderConfig: (hoistConfig: HoistToProviderConfig) => Builder<T, E>,
+  subscriptionKey: (subKey: AsyncStateKey) => Builder<T, E>,
+  selector: (selector: (
+    currentState: State<T>, lastSuccess: State<T>) => E) => Builder<T, E>,
+  areEqual: (areEqual: EqualityFn<E>) => Builder<T, E>,
+
+  build: (
+    subscriptionConfig: ExtendedUseAsyncStateConfiguration<T, E>,
+    dependencies: any[]
+  ) => UseAsyncStateReturnValue<T, E>
+}
+
+function BuilderImpl<T, E>(): Builder<T, E> {
+  const builder = Object.create(null);
+  const overrides = Object.create(null);
+
+  function applyOverride(name, value) {
+    overrides[name] = value;
+    return builder;
+  }
+
+  function build(
+    subscriptionConfig: ExtendedUseAsyncStateConfiguration<T, E>,
+    dependencies: any[]
+  ) {
+    return useAsyncStateImpl(subscriptionConfig, dependencies, overrides);
+  }
+
+  builder.initialValue = applyOverride.bind(null, "initialValue");
+  builder.producer = applyOverride.bind(null, "producer");
+  builder.key = applyOverride.bind(null, "key");
+  builder.source = applyOverride.bind(null, "source");
+  builder.runEffect = applyOverride.bind(null, "runEffect");
+  builder.runEffectDurationMs = applyOverride.bind(null, "runEffectDurationMs");
+  builder.payload = applyOverride.bind(null, "payload");
+  builder.lazy = applyOverride.bind(null, "lazy");
+  builder.fork = applyOverride.bind(null, "fork");
+  builder.condition = applyOverride.bind(null, "condition");
+  builder.hoistToProvider = applyOverride.bind(null, "hoistToProvider");
+  builder.forkConfig = applyOverride.bind(null, "forkConfig");
+  builder.hoistToProviderConfig = applyOverride.bind(null, "hoistToProviderConfig");
+  builder.subscriptionKey = applyOverride.bind(null, "subscriptionKey");
+  builder.selector = applyOverride.bind(null, "selector");
+  builder.areEqual = applyOverride.bind(null, "areEqual");
+
+  builder.build = build;
+
+  return builder;
+}
+
+useAsyncStateExport.auto = useAutoAsyncState;
+useAsyncStateExport.lazy = useLazyAsyncState;
+useAsyncStateExport.fork = useForkAsyncState;
+useAsyncStateExport.hoist = useHoistAsyncState;
+useAsyncStateExport.forkAuto = useForkAutoAsyncState;
+useAsyncStateExport.hoistAuto = useHoistAutoAsyncState;
+
+useAsyncStateExport.payload = curryProperty(payloadArgsToOverrides);
+useAsyncStateExport.selector = curryProperty(selectorArgsToOverrides);
+useAsyncStateExport.condition = curryProperty(conditionArgsToOverrides);
+
+useAsyncStateExport.builder = BuilderImpl;
 
 export const useAsyncState: <T, E>(
   subscriptionConfig: ExtendedUseAsyncStateConfiguration<T, E>,
