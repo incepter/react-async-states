89068aab879b0d07a5c76331f4297cb4790a6f98
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-02-21 22:23:39 +0100

    init

--- packages/async-state/types.ts
+++ packages/async-state/types.ts
@@ -50,7 +50,7 @@ export type ProducerSavedProps<T> = {
   lastSuccess?: State<T>
 }
 
-export type Producer<T> = (props: ProducerProps<T>) => T;
+export type Producer<T> = (props: ProducerProps<T>) => (T | Promise<T>);
 export type ProducerFunction<T> = (props: ProducerProps<T>) => AbortFn;
 
 export type ProducerConfig<T> = {
--- packages/react-async-states/src/__tests__/react-async-state/useAsyncState/subscription/outside-provider/index.test.tsx
+++ packages/react-async-states/src/__tests__/react-async-state/useAsyncState/subscription/outside-provider/index.test.tsx
@@ -1,43 +1,58 @@
 import * as React from "react";
-import {render, screen, act, fireEvent} from "@testing-library/react";
+import {act, fireEvent, render, screen} from "@testing-library/react";
 import {UseAsyncStateReturnValue} from "../../../../../types.internal";
 import {useAsyncState} from "../../../../../hooks/useAsyncState";
+import {AsyncStateStatus, State} from "../../../../../../../async-state";
 
-describe('should render initial value', () => {
-  it('should subscribe and get initial value, increment and decrement via run', async () => {
+describe('should subscribe -- sync', () => {
+  it('should subscribe and get initial value, increment and decrement sync via run and replace state', async () => {
     // given
     function Component() {
-      const {state, run, replaceState} : UseAsyncStateReturnValue<number, number> = useAsyncState({
+      const {
+        state,
+        run,
+        replaceState
+      }: UseAsyncStateReturnValue<number, number> = useAsyncState({
         producer(props): number {
           return props.args[0];
         },
         initialValue: 0,
         selector: d => d.data,
       });
+
       function increment() {
         run(state + 1);
       }
+
       function decrement() {
         run(state - 1);
       }
+
       function incrementReplaceState() {
         replaceState(old => old.data + 1);
       }
+
       function decrementReplaceState() {
         replaceState(old => old.data - 1);
       }
+
       return (
         <div>
           <button data-testid="increment" onClick={increment}>increment</button>
           <button data-testid="decrement" onClick={decrement}>decrement</button>
-          <button data-testid="increment-r" onClick={incrementReplaceState}>increment</button>
-          <button data-testid="decrement-r" onClick={decrementReplaceState}>decrement</button>
+          <button data-testid="increment-r"
+                  onClick={incrementReplaceState}>increment
+          </button>
+          <button data-testid="decrement-r"
+                  onClick={decrementReplaceState}>decrement
+          </button>
           <span data-testid="result">{state}</span>
-      </div>);
+        </div>);
     }
+
     // when
 
-    render(<Component />)
+    render(<Component/>)
 
     const incrementBtn = screen.getByTestId("increment");
     const decrementBtn = screen.getByTestId("decrement");
@@ -61,5 +76,84 @@ describe('should render initial value', () => {
     // -1
     fireEvent.click(decrementRBtn);
     expect(screen.getByTestId("result").innerHTML).toEqual("0");
+  });
+});
+
+describe('should subscribe -- async', () => {
+  it('should subscribe and get initial value, and perform async call', async () => {
+    // given
+    const pendingText = "loading...";
+
+    function Component() {
+      const {
+        state: {status, data},
+        run,
+        replaceState
+      }: UseAsyncStateReturnValue<number, State<number>> = useAsyncState({
+        producer(props): Promise<number> {
+          return new Promise<number>((resolve => {
+            let id = setTimeout(() => resolve(props.args[0]), 100);
+            props.onAbort(() => clearTimeout(id));
+          }));
+        },
+        initialValue: 0,
+      });
+
+      function increment() {
+        run(data + 1);
+      }
+
+      function decrement() {
+        run(data - 1);
+      }
+
+      const isPending = status === AsyncStateStatus.pending;
+      return (
+        <div>
+          <button data-testid="increment" onClick={increment}>increment</button>
+          <button data-testid="decrement" onClick={decrement}>decrement</button>
+          <span data-testid="pending">{isPending ? pendingText : ""}</span>
+          <span data-testid="result">{data}</span>
+        </div>);
+    }
+
+    // when
+
+    jest.useFakeTimers();
+    render(<Component/>)
+
+    const incrementBtn = screen.getByTestId("increment");
+    const decrementBtn = screen.getByTestId("decrement");
+    // then
+    expect(screen.getByTestId("result").innerHTML).toEqual("0");
+    expect(screen.getByTestId("pending").innerHTML).toEqual("");
+
+    // +1
+    act(() => {
+      fireEvent.click(incrementBtn);
+    });
+    expect(screen.getByTestId("result").innerHTML).toEqual("");
+    expect(screen.getByTestId("pending").innerHTML).toEqual(pendingText);
+
+    await act(async () =>{
+      await jest.advanceTimersByTime(100);
+    });
+
+    expect(screen.getByTestId("result").innerHTML).toEqual("1");
+    expect(screen.getByTestId("pending").innerHTML).toEqual("");
+
+    // -1
+    act(() => {
+      fireEvent.click(decrementBtn)
+    });
+    expect(screen.getByTestId("result").innerHTML).toEqual("");
+    expect(screen.getByTestId("pending").innerHTML).toEqual(pendingText);
+
+    await act(async () => {
+      await jest.advanceTimersByTime(100);
+    });
+
+    expect(screen.getByTestId("result").innerHTML).toEqual("0");
+    expect(screen.getByTestId("pending").innerHTML).toEqual("");
   });
 });
