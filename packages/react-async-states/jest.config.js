dc7c19ee72ec5bc8fea1c1f780d0640ca10d3de9
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-03-02 12:23:54 +0100

    fix

--- packages/react-async-states/jest.config.js
+++ packages/react-async-states/jest.config.js
@@ -2,12 +2,6 @@
 module.exports = async () => {
   return {
     verbose: true,
-    coverageDirectory: "build",
-    collectCoverageFrom: [
-      "**/*.{js,jsx,ts,tsx}",
-      "!**/node_modules/**",
-      "../async-state/**",
-      "../shared/**"
-    ],
+    coverageDirectory: "build"
   };
 };
