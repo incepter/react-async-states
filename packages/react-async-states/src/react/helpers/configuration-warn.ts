03a371e4fb7b4cd4278768bd0c681ab0169ac825
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-11-03 15:17:25 +0100

    fix

--- packages/react-async-states/src/react/helpers/configuration-warn.ts
+++ packages/react-async-states/src/react/helpers/configuration-warn.ts
@@ -1,61 +1,65 @@
+import {__DEV__} from "shared";
 import {
   SubscriptionMode,
   UseAsyncStateConfiguration
 } from "../../types.internal";
 import {computeCallerName} from "./useCallerName";
 
+let creationProperties, hoistProperties, forkProperties, irrelevantPropertiesByMode;
 
-const creationProperties = [
-  "producer",
-  "runEffect",
-  "cacheConfig",
-  "initialValue",
-  "skipPendingDelayMs",
-  "runEffectDurationMs",
-  "resetStateOnDispose",
-];
+if (__DEV__) {
+  creationProperties = [
+    "producer",
+    "runEffect",
+    "cacheConfig",
+    "initialValue",
+    "skipPendingDelayMs",
+    "runEffectDurationMs",
+    "resetStateOnDispose",
+  ];
+  hoistProperties = [
+    "hoistToProvider",
+    "hoistToProviderConfig",
+  ];
+  forkProperties = [
+    "fork",
+    "forkConfig",
+  ];
+  irrelevantPropertiesByMode = {
+    LISTEN: [...creationProperties, ...hoistProperties, ...forkProperties],
+    HOIST: [...forkProperties],
+    STANDALONE: [...hoistProperties, ...forkProperties],
+    WAITING: [...creationProperties, ...hoistProperties, ...forkProperties],
+    FORK: [...creationProperties, ...hoistProperties],
+    NOOP: [],
+    SOURCE: [...creationProperties, ...hoistProperties, ...forkProperties],
+    SOURCE_FORK: [...creationProperties, ...hoistProperties],
+    OUTSIDE_PROVIDER: [...hoistProperties, ...forkProperties],
+  };
+}
 
-const hoistProperties = [
-  "hoistToProvider",
-  "hoistToProviderConfig",
-];
 
-const forkProperties = [
-  "fork",
-  "forkConfig",
-];
-
-
-const irrelevantPropertiesByMode: Record<SubscriptionMode, string[]> = {
-  LISTEN: [...creationProperties, ...hoistProperties, ...forkProperties],
-  HOIST: [...forkProperties],
-  STANDALONE: [...hoistProperties, ...forkProperties],
-  WAITING: [...creationProperties, ...hoistProperties, ...forkProperties],
-  FORK: [...creationProperties, ...hoistProperties],
-  NOOP: [],
-  SOURCE: [...creationProperties, ...hoistProperties, ...forkProperties],
-  SOURCE_FORK: [...creationProperties, ...hoistProperties],
-  OUTSIDE_PROVIDER: [...hoistProperties, ...forkProperties],
-};
 
 export function warnInDevAboutIrrelevantUseAsyncStateConfiguration(
   mode: SubscriptionMode,
   userConfig: UseAsyncStateConfiguration<any, any>
 ) {
-  const irrelevantProperties = irrelevantPropertiesByMode[mode];
-  if (!irrelevantProperties.length) {
-    return;
-  }
+  if (__DEV__) {
+    const irrelevantProperties = irrelevantPropertiesByMode[mode];
+    if (!irrelevantProperties.length) {
+      return;
+    }
 
-  const usedIrrelevantProperties = irrelevantProperties.filter(
-    prop => userConfig[prop] !== undefined
-  )
+    const usedIrrelevantProperties = irrelevantProperties.filter(
+      prop => userConfig[prop] !== undefined
+    )
 
-  if (usedIrrelevantProperties.length) {
-    const caller = computeCallerName(9);
-    console.error(`[Incompatible configuration] - Subscription to '${userConfig.key}' ` +
-      `${userConfig.subscriptionKey ? '(with subscriptionKey=' +
-        userConfig.subscriptionKey + ') ' : ''}from '${caller}' is using incompatible ` +
-      `['${usedIrrelevantProperties.join(", ")}'] properties with its mode '${mode}'`);
+    if (usedIrrelevantProperties.length) {
+      const caller = computeCallerName(9);
+      console.error(`[Incompatible configuration] - Subscription to '${userConfig.key}' ` +
+        `${userConfig.subscriptionKey ? '(with subscriptionKey=' +
+          userConfig.subscriptionKey + ') ' : ''}from '${caller}' is using incompatible ` +
+        `['${usedIrrelevantProperties.join(", ")}'] properties with its mode '${mode}'`);
+    }
   }
 }
