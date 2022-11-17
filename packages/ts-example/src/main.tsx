b0d3065976b0509f93687b95e97be1b3c0d6685d
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-11-17 15:23:39 +0100

    fix

--- packages/ts-example/src/main.tsx
+++ packages/ts-example/src/main.tsx
@@ -4,26 +4,10 @@ import ReactDOM from "react-dom/client";
 import DevtoolsView from "react-async-states/dist/devtools/view"
 import EntryPoint from "./entryPoint";
 import "./styles/index.css";
-import {createSource, useSource} from "react-async-states";
-
-const counterSource = createSource("counter", null, {initialValue: 0});
-export default function DevModeApp() {
-  const {state} = useSource(counterSource);
-  return (
-    <div>
-      <hr/>
-      <button
-        onClick={() => counterSource.run(old => old.data + 1)}>{state.data}</button>
-      <hr/>
-    </div>
-  )
-}
-
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <EntryPoint/>
-    <DevModeApp/>
     <DevtoolsView/>
   </React.StrictMode>
 );
