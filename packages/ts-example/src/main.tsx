040259d2f5afa9c1f95a944a58da173477df130f
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-11-18 13:13:14 +0100

    start better readme

--- packages/ts-example/src/main.tsx
+++ packages/ts-example/src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {autoConfigureDevtools} from "async-states-devtools"
 
 import EntryPoint from "./entryPoint";
 import "./styles/index.css";
 import {createSource} from "react-async-states/src";
 
+import {autoConfigureDevtools} from "async-states-devtools"
 autoConfigureDevtools({open: true});
 
 createSource("demo", null, {initialValue: 0});
