42d87582b026f047c4985649e9ffcc06846f755b
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2022-11-26 17:32:59 +0100

    remove example project from source (keep only in my machine)

--- packages/example/src/past/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import './index.css'
-import App from './App';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(<React.Suspense fallback="app suspended"><App/></React.Suspense>);
-ReactDOM.render(<App/>, document.getElementById('root'));
