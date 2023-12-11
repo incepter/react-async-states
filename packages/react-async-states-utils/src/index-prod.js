32f82878f7c7af307ce7c41924919910064cc1ea
EL AYADI Mohamed <moh.el.ayadi@gmail.com>
2023-12-11 14:47:50 +0100

    add utils package deprecation warning & remove utils and fiber packages (#161)
    * add utils package deprecation warning
    
    * remove utils package
    
    * remove state-fiber package

--- packages/react-async-states-utils/src/index-prod.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./react-async-states-utils.production.js');
-} else {
-  module.exports = require('./react-async-states-utils.development.js');
-}
--- packages/react-async-states-utils/src/utils.ts
+++
@@ -1,7 +0,0 @@
-
-export function isFunction(fn): fn is Function {
-  return typeof fn === "function";
-}
-export function noop(): void {
-  // that's a noop fn
-}
