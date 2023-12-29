e01e4f8bd5e69e8564058cad6897280e78235cb3
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2023-12-29 23:10:26 +0100

    first application rewrite using proxies draft

---
+++ packages/react-async-states/src/__tests__/react-async-state/application2.test.tsx
@@ -0,0 +1,115 @@
+import * as React from "react";
+import { act, render, screen } from "@testing-library/react";
+import { createApplication2, Api } from "../../application/application2";
+import { flushPromises } from "../utils/test-utils";
+
+type User = {
+  id: number;
+  name: string;
+};
+
+let userSearch = async () => Promise.resolve({ id: 15, name: "incepter" });
+
+// current: api<User, [], Error>({
+//   eager: true,
+//   producer: userSearch,
+//   config: { runEffect: "debounce" },
+// }),
+
+type AppShape = {
+  auth: {
+    current: Api<User, [], Error>;
+  };
+  users: {
+    search: Api<User, [string], Error>;
+    findOne: Api<User, [string], Error>;
+  };
+};
+
+describe("createApplication2 abstraction tests", () => {
+  let originalConsoleError = console.error;
+  beforeAll(() => {
+    console.error = jest.fn();
+  });
+  afterAll(() => {
+    console.error = originalConsoleError;
+  });
+
+  it("should create the application with correct types", () => {
+    let app = createApplication2<AppShape>(undefined, {});
+    let authCurrent: Api<User, [], Error> = app.auth.current;
+    let searchToken: Api<User, [string], Error> = app.users.search;
+  });
+
+  it("should throw if used without being injected", () => {
+    let errorMessageToThrow =
+      "Call app.users.search.define(producer) before using app.users.search";
+    let app = createApplication2<AppShape>(undefined, {});
+    expect(() => app.users.search()).toThrow(errorMessageToThrow);
+    expect(() => app.users.search.useData()).toThrow(errorMessageToThrow);
+    expect(() => app.users.search.useAsync()).toThrow(errorMessageToThrow);
+    // the following was eager
+    // expect(() => app.auth.current.useAsyncState()).not.toThrow(
+    //   errorMessageToThrow
+    // );
+  });
+
+  it("should be able to inject producer and configuration and then not throw", () => {
+    let app = createApplication2<AppShape>(undefined, {});
+
+    // should throw because it isn't injected yet
+    expect(() => app.users.search()).toThrow(
+      "Call app.users.search.define(producer) before using app.users.search"
+    );
+    app.users.search.define(userSearch, { skipPendingDelayMs: 400 });
+    expect(app.users.search().getConfig()).toEqual({
+      context: {},
+      skipPendingDelayMs: 400,
+    });
+  });
+  it("should reuse the same instance when injecting several times", () => {
+    let app = createApplication2<AppShape>(undefined, {});
+
+    let src = app.users.search.define(userSearch)();
+    let src2 = app.users.search.define(userSearch)();
+    expect(src).toBe(src2);
+  });
+  it("should subscribe to a created api in component using use (auto run)", async () => {
+    let app = createApplication2<AppShape>(undefined, {});
+    app.users.search.define(userSearch);
+
+    function Component() {
+      let { data } = app.users.search.useData({
+        lazy: false,
+        condition: (s) => s.status === "initial",
+      });
+      return <span data-testid="data">{data?.name}</span>;
+    }
+
+    render(
+      <React.StrictMode>
+        <React.Suspense fallback={<div data-testid="pending">pending</div>}>
+          <Component />
+        </React.Suspense>
+      </React.StrictMode>
+    );
+    expect(screen.getByTestId("pending").innerHTML).toBe("pending");
+    await act(async () => await flushPromises());
+    expect(screen.getByTestId("data").innerHTML).toBe("incepter");
+  });
+  it("should subscribe to a created api in component using useAsyncState", async () => {
+    let app = createApplication2<AppShape>(undefined, {});
+
+    function Component() {
+      let { state } = app.users.findOne.define(userSearch).useAsync();
+      return <span data-testid="status">{state.status}</span>;
+    }
+
+    render(
+      <React.StrictMode>
+        <Component />
+      </React.StrictMode>
+    );
+    expect(screen.getByTestId("status").innerHTML).toBe("initial");
+  });
+});
---
+++ packages/react-async-states/src/application/application2.ts
@@ -0,0 +1,192 @@
+import { UseAsyncState, UseConfig } from "../types.internal";
+import { __DEV__, assign, emptyArray } from "../shared";
+import { __DEV__setHookCallerName } from "../hooks/modules/HookSubscription";
+import { useCallerName } from "../helpers/useCallerName";
+import { useData_internal } from "../hooks/useData_internal";
+import { useAsync_internal } from "../hooks/useAsync_internal";
+import {
+  createSource,
+  Producer,
+  ProducerConfig,
+  Source,
+  State,
+  createContext,
+} from "async-states";
+import { LegacyHookReturn, ModernHookReturn } from "../hooks/types";
+
+export type Api<TData, TArgs extends unknown[] = [], TError = Error> = {
+  (): Source<TData, TArgs, TError>;
+  define(
+    fn: Producer<TData, TArgs, TError>,
+    config?: ProducerConfig<TData, TArgs, TError>
+  ): Api<TData, TArgs, TError>;
+  useData<S = State<TData, TArgs, TError>>(
+    config?: UseConfig<TData, TArgs, TError, S>,
+    deps?: any[]
+  ): ModernHookReturn<TData, TArgs, TError, S>;
+  useAsync<S = State<TData, TArgs, TError>>(
+    config?: UseConfig<TData, TArgs, TError, S>,
+    deps?: any[]
+  ): LegacyHookReturn<TData, TArgs, TError, S>;
+};
+type AnyApi = Api<any, any, any>;
+type AppShape = Record<string, Record<string, AnyApi>>;
+
+type App<TApp extends AppShape> = {
+  [resource in keyof TApp]: Resource<TApp[resource]>;
+};
+type Resource<TResource extends Record<string, AnyApi>> = {
+  [api in keyof TResource]: TResource[api];
+};
+
+export function createApplication2<TApp extends AppShape>(
+  initialShape?: any,
+  context?: object
+): App<TApp> {
+  let app: Partial<App<TApp>> = {};
+
+  return new Proxy(app as App<TApp>, {
+    get(_: App<TApp>, property: string): any {
+      let resourceName = property as keyof TApp;
+
+      let existingResource = app[resourceName];
+      if (existingResource) {
+        return existingResource;
+      }
+
+      if (context) {
+        createContext(context);
+      }
+      let resource = createResource<TApp, typeof resourceName>(
+        resourceName,
+        context
+      );
+
+      app[resourceName] = resource;
+      return resource;
+    },
+  });
+}
+
+function createResource<TApp extends AppShape, TRes extends keyof App<TApp>>(
+  resourceName: TRes,
+  context?: object
+): Resource<App<TApp>[TRes]> {
+  let resource: Partial<Resource<TApp[TRes]>> = {};
+
+  return new Proxy(resource as Resource<TApp[TRes]>, {
+    get(target: Resource<TApp[TRes]>, property: string): any {
+      let apiName = property as keyof App<TApp>[TRes];
+
+      let exitingApi = resource[property];
+      if (exitingApi) {
+        return exitingApi;
+      }
+
+      let api = createApi<TApp, TRes, typeof apiName>(
+        resourceName,
+        apiName,
+        context
+      );
+
+      resource[apiName] = api;
+      return api;
+    },
+  });
+}
+
+function createApi<
+  TApp extends AppShape,
+  TRes extends keyof App<TApp>,
+  TApi extends keyof App<TApp>[TRes],
+>(
+  resourceName: TRes,
+  apiName: TApi,
+  context?: object
+): TApp[TRes][typeof apiName] {
+  // ): App<TApp>[TRes][typeof apiName] {
+  type TData = InferData<TApp, TRes, TApi>;
+  type TArgs = InferArgs<TApp, TRes, TApi>;
+  type TError = InferError<TApp, TRes, TApi>;
+
+  let source: Source<TData, TArgs, TError> | null = null;
+
+  function token() {
+    if (!source) {
+      let path = `app.${String(resourceName)}.${String(apiName)}`;
+      throw new Error(`Call ${path}.define(producer) before using ${path}`);
+    }
+    return source;
+  }
+  token.define = define;
+  token.useData = (useDataForApp<TData, TArgs, TError>).bind(null, token);
+  token.useAsync = (useAsyncForApp<TData, TArgs, TError>).bind(null, token);
+
+  return token as TApp[TRes][typeof apiName];
+
+  function define(
+    producer: Producer<TData, TArgs, TError> | null,
+    config?: ProducerConfig<TData, TArgs, TError>
+  ) {
+    if (!source) {
+      let key = `${String(resourceName)}_${String(apiName)}`;
+      let apiConfig = config;
+      if (context !== null) {
+        apiConfig = assign({}, apiConfig, { context });
+      }
+      source = createSource(key, producer, apiConfig);
+    } else {
+      source.replaceProducer(producer);
+      source.patchConfig(config);
+    }
+    return token;
+  }
+}
+
+function useDataForApp<TData, TArgs extends unknown[], TError>(
+  token: Api<TData, TArgs, TError>,
+  config?: UseConfig<TData, TArgs, TError>,
+  deps?: any[]
+) {
+  let source = token();
+
+  if (__DEV__) {
+    __DEV__setHookCallerName(useCallerName(3));
+  }
+
+  return useData_internal(source, deps || emptyArray, config);
+}
+
+function useAsyncForApp<TData, TArgs extends unknown[], TError>(
+  token: Api<TData, TArgs, TError>,
+  config?: UseConfig<TData, TArgs, TError>,
+  deps?: any[]
+) {
+  let source = token();
+
+  if (__DEV__) {
+    __DEV__setHookCallerName(useCallerName(3));
+  }
+
+  return useAsync_internal(source, deps || emptyArray, config);
+}
+
+type InferData<
+  TApp extends AppShape,
+  TRes extends keyof App<TApp>,
+  TApi extends keyof App<TApp>[TRes],
+> = TApp[TRes][TApi] extends Api<infer T, any, any> ? T : never;
+
+type InferArgs<
+  TApp extends AppShape,
+  TRes extends keyof App<TApp>,
+  TApi extends keyof App<TApp>[TRes],
+> = App<TApp>[TRes][TApi] extends Api<any, infer A extends unknown[]>
+  ? A
+  : never;
+
+type InferError<
+  TApp extends AppShape,
+  TRes extends keyof App<TApp>,
+  TApi extends keyof App<TApp>[TRes],
+> = App<TApp>[TRes][TApi] extends Api<any, any, infer E> ? E : never;
